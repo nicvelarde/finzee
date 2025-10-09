c5d5d6eb93ac3749710c94395cc7af464050cbb8
nicvelarde <88861887+nicvelarde@users.noreply.github.com>
2025-10-09 00:35:11 +0000

    Added a nice scroll and fade in

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById("clickMe").addEventListener("click", function() {
-  alert("You just clicked a button on your first GitHub website!");
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('visible');
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
 });
